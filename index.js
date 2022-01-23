eef00d6b1d507d65bb8a0ec425ef6666b359af60
Shashank Araokar <shashank.araokar@gmail.com>
2022-01-23 14:23:34 -0500

    Fixed logic

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const dns = require('dns')
+const os = require('os');
 const app = express()
 const port = 80
 
-dns.lookup('localhost', function(err, result) {
+dns.lookup(os.hostname(), function(err, result) {
   console.log("My ip address is: " + result);
   app.get('/', (req, res) => {
     res.send('IP: ' + result);
